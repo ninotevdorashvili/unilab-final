6625954a10f917c53b367d38d6f53456d81c4391
ninotevdorashvili <84516383+ninotevdorashvili@users.noreply.github.com>
2021-09-06 05:38:17 +0400

    add footer and scroll effect

--- js/script.js
+++ js/script.js
@@ -103,3 +103,48 @@ $('.center').slick({
     rawcard.style.backgroundImage = '';
   }, false); 
 
+// scroll-effect
+const scrollElements = document.querySelectorAll('.scroll');
+scrollElements.forEach((el) => {
+    el.style.opacity = 0
+  })
+const elementInView = (el, scrollOffset = 0) => {
+    const elementTop = el.getBoundingClientRect().top;
+    return (
+      elementTop <= 
+      ((window.innerHeight || document.documentElement.clientHeight) - scrollOffset)
+    );
+  } 
+const displayScrollElement = (element) => {
+  element.classList.add('scrolled');
+
+}
+const hideScrollElement = (element) => {
+  element.classList.remove('scrolled');
+}
+ 
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 100)) {
+      displayScrollElement(el);
+    } else {
+      hideScrollElement(el);
+    }
+  })
+};
+window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+})
+
+
+
+// header-scroll
+$(function() {
+  $(window).on("scroll", function() {
+      if($(window).scrollTop() > 50) {
+          $(".header").addClass("bg-active");
+      } else {
+         $(".header").removeClass("bg-active");
+      }
+  });
+});
